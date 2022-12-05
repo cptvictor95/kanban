1b8a63a69f6f004121843bb1f69b511e8c81a7ac
cptvictor95 <cpt.victor@hotmail.com>
2022-12-04 22:42:03 -0300

    fix header on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export const Header: React.FC = () => {
   const router = useRouter();
 
   return (
-    <nav className="flex h-20 w-full items-center justify-between px-8">
+    <nav className="flex w-full flex-col items-center justify-between gap-2 px-8 py-4 md:flex-row">
       <button
         onClick={() => router.push("/")}
         className="flex items-center gap-2 rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
@@ -27,25 +27,24 @@ export const Header: React.FC = () => {
         )}
       </button>
 
-      <p className="flex items-center gap-2 text-center text-xl text-white">
-        {sessionData && (
+      <button
+        className="flex items-center gap-4 rounded-full bg-white/10 px-8 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => signOut() : () => signIn()}
+      >
+        {sessionData ? (
           <>
-            {sessionData.user?.name}
+            Sign out
             <Image
               className="rounded-full"
-              width="40"
-              height="40"
+              width="32"
+              height="32"
               src={sessionData.user?.image as string}
               alt={`${sessionData.user?.name}'s avatar`}
             />
           </>
+        ) : (
+          "Sign in"
         )}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => signOut() : () => signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
       </button>
     </nav>
   );
--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -9,6 +9,12 @@ const Main: React.FC<{ children: React.ReactNode; title?: string }> = ({
     <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#3b0129] to-[#2c011f]">
       <Head>
         <title>{title ?? "Kanban"}</title>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+        />
         <meta name="description" content="Created by Victor Torres" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
